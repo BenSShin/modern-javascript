789415914c78fcfb47248c29bdee393951053ac0
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-02 12:32:13 -0500

    installed webpack and mathjs

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const moment = require("moment");
+const math = require("mathjs");
+
 console.log("Hello World");
 
 var datetime = moment().startOf("day").fromNow();
 console.log(datetime);
+
+console.log(math.sqrt(-4));
